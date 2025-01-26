06ffcd4ab4a15d8c1b2d4a01f8aefc2ae73057a2
unknown <fatahuddin3@gmail.com>
2025-01-26 17:58:45 +0600

    all

---
+++ FRONT/frontendback/src/App.jsx
@@ -0,0 +1,28 @@
+
+// src/App.js main code
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+
+function App() {
+    return (
+
+        <Router>
+            <div className="App">
+
+                <Routes>
+
+
+
+
+
+
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+
+    );
+}
+export default App;
