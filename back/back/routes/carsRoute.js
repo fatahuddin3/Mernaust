f5033c88f16ecde9081ce314a408f51ad2086e4a
unknown <fatahuddin3@gmail.com>
2025-02-15 15:11:53 +0600

    carroutes2

--- back/back/routes/carsRoute.js
+++ back/back/routes/carsRoute.js
@@ -34,6 +34,35 @@ router.get('/uploads/:filename', (req, res) => {
         res.status(404).send('Image not found');
     }
 });
+router.get("/getallcars", async (req, res) => {
+    try {
+        const cars = await Cars.find();
+        res.send(cars);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+});
+router.get("/images", async (req, res) => {
+    try {
+        const { fueltype } = req.query;
+
+        let query = {};
+        if (fueltype) {
+            query.fueltype = fueltype;
+        }
+
+        const cars = await Cars.find(query, "image name fueltype rentperhour capacity");
+
+        if (!cars.length) {
+            return res.status(404).json({ error: `No cars found for ${fueltype}` });
+        }
+
+        res.status(200).json(cars);
+    } catch (error) {
+        console.error("Error fetching cars:", error);
+        res.status(500).json({ error: "Server error fetching car data" });
+    }
+});
 
 
 
@@ -81,6 +110,71 @@ router.post('/car', upload.single('image'), async (req, res) => {
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+router.put('/:id', upload.single('image'), async (req, res) => {
+
+    try {
+        const car = await Cars.findOne({ _id: req.params.id });
+        if (!car) {
+            return res.status(404).json({ message: 'Car not found' });
+        }
+
+        const { name, fueltype, capacity, rentperhour } = req.body;
+
+        car.name = name || car.name;
+        car.fueltype = fueltype || car.fueltype;
+        car.capacity = capacity || car.capacity;
+        car.rentperhour = rentperhour || car.rentperhour;
+
+        if (req.file) {
+            const oldImagePath = car.image;
+            if (oldImagePath && fs.existsSync(path.join(__dirname, oldImagePath))) {
+                fs.unlinkSync(path.join(__dirname, oldImagePath));
+            }
+            car.image = `/uploads/${req.file.filename}`;
+        }
+
+        await car.save();
+
+        return res.status(200).json({
+            message: 'Car updated successfully',
+            car: {
+                _id: car._id,
+                name: car.name,
+                fueltype: car.fueltype,
+                rentperhour: car.rentperhour,
+                capacity: car.capacity,
+                image: car.image,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+});
+
+router.delete('/deletecar/:id', async (req, res) => {
+    try {
+
+
+
+
+        const car = await Patient.findByIdAndDelete(req.params.id);
+
+        if (!car) {
+            return res.status(404).json({ message: 'Car not found' });
+        }
+
+        if (car.image && fs.existsSync(car.image)) {
+            fs.unlinkSync(car.image);
+        }
+
+        return res.status(200).json({ message: 'Car deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error', error });
     }
 });
 
